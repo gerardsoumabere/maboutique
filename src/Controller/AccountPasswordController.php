0f1563924335f5fa852847ce932880eb9665b626
soumabere <contact@gerard-soumabere.fr>
2021-12-12 16:01:33 +0100

    Initial commit before Esay Admin Install

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountPasswordController extends AbstractController
+{
+    #[Route('/compte/modifier-mon-mot-de-passe', name: 'account_password')]
+    public function index(Request $request,ManagerRegistry $doctrine,UserPasswordHasherInterface $encoder): Response
+    {
+        $notification = null;
+
+        $user= $this->getUser();
+
+        $form = $this->createForm(ChangePasswordType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd =$form->get('old_password')->getData();
+
+            if($encoder->isPasswordValid($user,$old_pwd)){
+                $new_pwd=$form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user,$new_pwd);
+
+                $user->setPassword($password);
+
+                $entityManager=$doctrine->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $notification = "Votre mot de passe a été mise a jour.";
+
+            } else {
+                $notification = "Votre mot de passe n'est pas le bon.";
+            }
+        }
+
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView(),
+            'notification'=>$notification
+        ]);
+    }
+}
