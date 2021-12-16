a83920d41e8b77d8ca80b798feafdbda2d258c84
soumabere <contact@gerard-soumabere.fr>
2021-12-16 17:19:28 +0100

    Passage à la vidéo 40

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,15 +2,19 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
+    private $entityManager ;
 
-    public  function __construct(SessionInterface $session )
+    public  function __construct(EntityManagerInterface $entityManager, SessionInterface $session )
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id)
@@ -35,5 +39,47 @@ class Cart
     public function remove()
     {
         return $this->session->remove('cart');
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart',[]);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart',$cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart',[]);
+
+        if($cart[$id]>1){
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+
+        $this->session->set('cart',$cart);
+    }
+
+    public function getFull(){
+
+        $cartComplete = [];
+
+        foreach ( $this->get() as $id => $quantity){
+            $product_object = $this->entityManager->getRepository(Product::class)->findOneById($id);
+
+            if (!$product_object){
+                $this->delete($id);
+                continue;
+            }
+            $cartComplete[] = [
+                'product' => $product_object,
+                'quantity' =>  $quantity
+            ];
+        }
+
+        return $cartComplete;
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -12,27 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
     #[Route('/mon-panier', name: 'cart')]
     public function index(Cart $cart): Response
     {
-        $cartComplete = [];
-
-        foreach ( $cart->get() as $id => $quantity){
-            $cartComplete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' =>  $quantity
-            ];
-        }
-
-
         return $this->render('cart/index.html.twig',[
-           'cart'=>$cartComplete
+           'cart'=>$cart->getFull()
         ]);
     }
 
@@ -48,6 +32,21 @@ class CartController extends AbstractController
     public function remove(Cart $cart): Response
     {
         $cart->remove();
+        return $this->redirectToRoute('products');
+    }
+
+    #[Route('/cart/delete/{id}', name: 'delete_to_cart')]
+    public function delete(Cart $cart,$id): Response
+    {
+        $cart->delete($id);
         return $this->redirectToRoute('cart');
     }
+
+    #[Route('/cart/decrease/{id}', name: 'decrease_to_cart')]
+    public function decrease(Cart $cart,$id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
+    }
+
 }
